2220febf08e8a96e8a18fd48d7d0dff17f486d15
panders23 <github-iM6aiPei@panders.de>
2016-06-22 23:04:50 +0200

    /SURFRAW_text_browser/s/lib/libs/

--- sh/surfraw-pager.sh
+++ sh/surfraw-pager.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ~/.surfraw-pager.conf
-# SURFRAW_text_browser="<path to utils.git>/sh/lib/surfraw-pager.lib.sh"
+# SURFRAW_text_browser="<path to utils.git>/sh/libs/surfraw-pager.lib.sh"
 # SURFRAW_graphical=no
 
 [[ -f ~/.surfraw-pager.conf ]] \
