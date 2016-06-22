29e908afa7af30b524a63d7d787f643f02b42ce5
panders23 <github-iM6aiPei@panders.de>
2016-06-22 23:00:05 +0200

    add surfraw-pager (calls w3m, less)

---
+++ sh/libs/surfraw-pager.lib.sh
@@ -0,0 +1,1 @@
+w3m -dump $@ | less
---
+++ sh/surfraw-pager.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# ~/.surfraw-pager.conf
+# SURFRAW_text_browser="<path to utils.git>/sh/lib/surfraw-pager.lib.sh"
+# SURFRAW_graphical=no
+
+[[ -f ~/.surfraw-pager.conf ]] \
+    && . ~/.surfraw-pager.conf
+
+surfraw $@
